6cc15753384ce4189d27ffde9818f415a6c07d76
Azula <azula@aero.vi>
2026-06-01 13:18:19 -0400

    fix: hotel stats - 65 bookings / 100 stays (not reversed)

--- js/app.js
+++ js/app.js
@@ -1030,7 +1030,7 @@ function buildSummaryContent(yearFilter) {
     html += buildStatCard('\u{1F6A2}', 'Cruises', cruises.length, cruiseDays + ' days at sea');
     html += buildStatCard('\u2708\uFE0F', 'Flights', flights.length, flightBookings.size + ' bookings');
     html += buildStatCard('\u{1F686}', 'Trains', trains.length, '');
-    html += buildStatCard('\u{1F3E8}', 'Hotels', hotels.length, hotelBookings.size + ' bookings, ' + hotelNights + ' nights');
+    html += buildStatCard('\u{1F3E8}', 'Hotels', hotelBookings.size + ' bookings', hotels.length + ' stays, ' + hotelNights + ' nights');
     html += buildStatCard('\u2693', 'Ports', ports.size, '');
     html += buildStatCard('\u{1F3AB}', 'Events', events.length, '');
     html += '</div>';
@@ -1143,7 +1143,7 @@ function renderStats(trips) {
         + '<span class="stat">\u{1F6A2} <strong>' + cruises + '</strong> Cruises</span>'
         + '<span class="stat">\u2708\uFE0F <strong>' + bookingCount + '</strong> Bookings / <strong>' + flightCount + '</strong> Flights</span>'
         + '<span class="stat">\u{1F686} <strong>' + trains + '</strong> Trains</span>'
-        + '<span class="stat">\u{1F3E8} <strong>' + hotelBookingCount + '</strong> Bookings / <strong>' + hotelCount + '</strong> Hotels</span>'
+        + '<span class="stat">\u{1F3E8} <strong>' + hotelBookingCount + '</strong> Bookings / <strong>' + hotelCount + '</strong> Stays</span>'
         + '<span class="stat">\u{1F3AB} <strong>' + events + '</strong> Events</span>'
         + (issueCount > 0 ? '<span class="stat warning">\u26A0\uFE0F <strong>' + issueCount + '</strong> Issues</span>' : '');
 }
