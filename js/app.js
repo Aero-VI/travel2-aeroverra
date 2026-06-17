4dfd7a998be7c15601b9a948f969e414a1301f7b
Azula <azula@aero.vi>
2026-06-17 10:08:59 +0000

    chore: auto cache-bust [skip ci]

--- js/app.js
+++ js/app.js
@@ -1849,7 +1849,7 @@ function setupSearch() {
 async function init() {
     try {
         const [tripsRes, eventsRes] = await Promise.all([
-            fetch('data/trips.json?h=26cb2904'),
+            fetch('data/trips.json?h=bde4f459'),
             fetch('data/events.json?h=6c07e64f')
         ]);
         tripsData = await tripsRes.json();
